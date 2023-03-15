c2e1fd29836fbf58e91a4e6bccabf7383deebe24
dan <phuhuong646@gmail.com>
2023-03-15 17:12:03 +0700

    "Spring data jpa testing"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import com.github.javafaker.Faker;
 import java.util.List;
 
 @SpringBootApplication
@@ -12,5 +12,72 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner commandLineRunner(
+			StudentRepository studentRepository,
+			StudentIdCardRepository studentIdCardRepository) {
+		return args -> {
+			Faker faker = new Faker();
 
+			String firstName = faker.name().firstName();
+			String lastName = faker.name().lastName();
+			String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+			Student student = new Student(
+					firstName,
+					lastName,
+					email,
+					faker.number().numberBetween(17, 55));
+
+			student.addBook(
+					new Book("Clean Code", LocalDateTime.now().minusDays(4)));
+
+			student.addBook(
+					new Book("Think and Grow Rich", LocalDateTime.now()));
+
+			student.addBook(
+					new Book("Spring Data JPA", LocalDateTime.now().minusYears(1)));
+
+			StudentIdCard studentIdCard = new StudentIdCard(
+					"123456789",
+					student);
+
+			student.setStudentIdCard(studentIdCard);
+
+			student.addEnrolment(new Enrolment(
+					new EnrolmentId(1L, 1L),
+					student,
+					new Course("Computer Science", "IT"),
+					LocalDateTime.now()
+			));
+
+			student.addEnrolment(new Enrolment(
+					new EnrolmentId(1L, 2L),
+					student,
+					new Course("Amigoscode Spring Data JPA", "IT"),
+					LocalDateTime.now().minusDays(18)
+			));
+
+			student.addEnrolment(new Enrolment(
+					new EnrolmentId(1L, 2L),
+					student,
+					new Course("Amigoscode Spring Data JPA", "IT"),
+					LocalDateTime.now().minusDays(18)
+			));
+
+
+
+			studentRepository.save(student);
+
+			studentRepository.findById(1L)
+					.ifPresent(s -> {
+						System.out.println("fetch book lazy...");
+						List<Book> books = student.getBooks();
+						books.forEach(book -> {
+							System.out.println(
+									s.getFirstName() + " borrowed " + book.getBookName());
+						});
+					});
+
+		};
+	}
 }
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,146 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "student")
+@Table(
+        name = "student",
+        uniqueConstraints ={
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(
+            name = "id"
+    )
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String email;
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private Integer age;
+    @OneToOne(
+            mappedBy = "student",
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST,CascadeType.REMOVE}
+    )
+    private StudentIdCard studentIdCard;
+
+    @OneToMany(
+            mappedBy = "student",
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST,CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+
+    )
+    private List<Book> books = new ArrayList<>();
+
+    public Student(){}
+
+    public Student(String firstName,
+                   String lastName,
+                   String email,
+                   Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setStudentIdCard(StudentIdCard studentIdCard) {
+        this.studentIdCard = studentIdCard;
+    }
+    public void addBookToList(Book book){
+        if(!this.books.contains(book)){
+            this.books.add(book);
+            book.setStudent(this);
+        }
+    }
+    public void  removeBookFromLish(Book book){
+        if(this.books.add(book)){
+            this.books.remove(book);
+            book.setStudent(null);
+        }
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
